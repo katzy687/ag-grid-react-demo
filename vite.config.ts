d6bce3cefc7ca11f08a2b238094714207ececcd6
katzy687 <katzy687@gmail.com>
2023-08-11 19:33:50 +0300

    fixed vite config bug

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
   /// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { configDefaults } from 'vitest/config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
     exclude: [
+      ...configDefaults.exclude,
       "e2e/*" // playwright will run these
     ]
   }
